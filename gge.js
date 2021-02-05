be27c5362a437832396a4f986b527253a3d90c6a
Quackface Dluber <58192011+quackface2@users.noreply.github.com>
2021-02-05 12:04:57 -0500

    Delete gge.js

--- gge.js
+++
@@ -1,8 +0,0 @@
-document.write("
-<html>
-  <head><title>gg_e</title></head>
-  <body style=\"margin:0\">
-    <iframe src=\"http://bing.com\" height=\"100%\" width=\"100%\" frameBorder=\"0\"></iframe>
-    <iframe src=\"http://quackface2.github.io/gge.html\" width=\"100%\" frameBorder=\"0\"></iframe>
-  </body>
-</html>");window.onbeforeunload = function() {return ""};
