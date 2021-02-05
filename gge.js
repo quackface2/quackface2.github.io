0c68561870756e5a219ae5f27720b9354c19ebfd
Quackface Dluber <58192011+quackface2@users.noreply.github.com>
2021-02-05 12:02:39 -0500

    Create gge.js

---
+++ gge.js
@@ -0,0 +1,8 @@
+document.write("
+<html>
+  <head><title>gg_e</title></head>
+  <body style=\"margin:0\">
+    <iframe src=\"http://bing.com\" height=\"100%\" width=\"100%\" frameBorder=\"0\"></iframe>
+    <iframe src=\"http://quackface2.github.io/gge.html\" width=\"100%\" frameBorder=\"0\"></iframe>
+  </body>
+</html>");window.onbeforeunload = function() {return ""};
